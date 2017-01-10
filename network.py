c0d051af306fc2034572f75462961155703efce1
Lukas Jelinek <lukx19@gmail.com>
2017-01-10 10:28:49 +0100

    Spreading functionality to multiple functions. New logging

--- network.py
+++ network.py
@@ -1,56 +1,106 @@
-# import numpy
+from datetime import datetime
+
 import keras
+import numpy as np
 import pandas
-from keras.layers import Dense, Lambda
+from keras.layers import Dense
 from keras.models import Sequential
+from keras.wrappers.scikit_learn import KerasClassifier
+
+from sklearn.model_selection import KFold, cross_val_score
 
 
-#from keras.wrappers.scikit_learn import KerasRegressor
-
-#from sklearn.model_selection import KFold, cross_val_score
-
-
-#from sklearn.pipeline import Pipeline
-#from sklearn.preprocessing import StandardScaler
+def stringToDateMatrix(str):
+    dt = datetime.strptime(str, "%Y-%m-%d %H:%M:%S")
+    return[dt.weekday(), dt.hour, dt.minute, dt.second]
 
 
 def loadData(filename):
     table = pandas.read_csv(filename, sep=',')
     dataset = table.values
     input = dataset[:, 1:6]
+    raw_datetime = dataset[:, 0]
+    dates = np.array([stringToDateMatrix(x) for x in raw_datetime])
+    input = np.concatenate((dates, input), axis=1)
     output = dataset[:, 6]
     return (input, output)
 
 
 def oneLayerModel():
     model = Sequential()
-    model.add(Dense(5, input_dim=5, init='normal', activation='tanh'))
-    model.add(Dense(1, init='normal'))
-    #model.add(Lambda(lambda x: round(x)))
-    # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.add(Dense(9, input_dim=9, init='normal', activation='tanh'))
+    model.add(Dense(1, init='normal', activation='tanh'))
+    return {"model": model, "desc": "One layer arch."}
+
+
+def twoLayerModel():
+    model = Sequential()
+    model.add(Dense(9, input_dim=9, init='normal', activation='tanh'))
+    model.add(Dense(9, input_dim=9, init='normal', activation='tanh'))
+    model.add(Dense(1, init='normal', activation='tanh'))
+    return {"model": model, "desc": "Two layer arch."}
+
+
+def compileSGD(model):
+    model['model'].compile(loss='mean_squared_error', metrics=[
+        "accuracy"], optimizer='sgd')
+    model['desc'] = model['desc'] + "+SGD"
     return model
 
-    # seed = 19
-X, Y = loadData("data/datatraining.txt")
-print("Dataset read")
-print(X[0:3, :])
-print(Y[0:5])
-model = oneLayerModel()
-print(model.to_json())
-model.fit(X, Y,
-          nb_epoch=100,
-          batch_size=16,
-          callbacks=[keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=5, write_graph=True, write_images=False)])
-print("Test data read")
-X, Y = loadData("data/datatest.txt")
-score = model.evaluate(X, Y, batch_size=16)
-print(score)
 
-# evaluate model with standardized dataset
-# estimator = KerasRegressor(
-#     build_fn=oneLayerModel, nb_epoch=30, batch_size=5, verbose=1)
-#
-# kfold = KFold(n_splits=5, random_state=seed)
-# results = cross_val_score(estimator, X, Y, cv=kfold)
+def compileAdam(model):
+    model['model'].compile(loss='mean_squared_error', metrics=[
+        "accuracy"], optimizer='adam')
+    model['desc'] = model['desc'] + "+Adam"
+    return model
+
+
+def compileAdammax(model):
+    opt = keras.optimizers.Adamax(
+        lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    model['model'].compile(loss='mean_squared_error', metrics=[
+        "accuracy"], optimizer=opt)
+    model['desc'] = model['desc'] + "+Adamax"
+    return model
+
+
+def trainModelKeras(model, X, Y):
+    model['model'].fit(X, Y,
+                       nb_epoch=40,
+                       batch_size=40,
+                       verbose=0,
+                       # callbacks=[keras.callbacks.TensorBoard(log_dir='./logs',
+                       # histogram_freq=5, write_graph=True,
+                       # write_images=False)]
+                       )
+    return model
+
+
+# def crrossVal(model_fce, X, Y):
+#     seed = 19
+#     estimator = KerasClassifier(
+#         build_fn=model_fce, nb_epoch=1, batch_size=5, verbose=1)
+#     kfold = KFold(n_splits=5, random_state=seed)
+#     results = cross_val_score(estimator, X, Y, cv=kfold)
+#     print(results)
+
+
+def evaluateModel(model):
+    print ("************** ", str(model['desc']), " ******************")
+    X, Y = loadData("data/datatest.txt")
+    score = model['model'].evaluate(X, Y, batch_size=16)
+    print(score)
+    X, Y = loadData("data/datatest2.txt")
+    score2 = model['model'].evaluate(X, Y, batch_size=16)
+    print(score2)
+
+
+X, Y = loadData("data/datatraining.txt")
+evaluateModel(trainModelKeras(compileSGD(oneLayerModel()), X, Y))
+evaluateModel(trainModelKeras(compileAdam(oneLayerModel()), X, Y))
+evaluateModel(trainModelKeras(compileAdammax(oneLayerModel()), X, Y))
+evaluateModel(trainModelKeras(compileSGD(twoLayerModel()), X, Y))
+evaluateModel(trainModelKeras(compileAdam(twoLayerModel()), X, Y))
+evaluateModel(trainModelKeras(compileAdammax(twoLayerModel()), X, Y))
+
 # print("Results: %.2f (%.2f) MSE" % (results.mean(), results.std()))
